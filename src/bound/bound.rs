09801203470a23d3b8bcf608c5e7f780aaa362bb
Pawan Singh Bisht <46450720+bishtpawan@users.noreply.github.com>
2019-12-04 16:34:39 +0530

    update bound

--- src/bound/bound.rs
+++ src/bound/bound.rs
@@ -1,10 +1,14 @@
 use crate::introduction::intro::{Calculator, Data};
 
-/*pub fn calculate(item: impl Calculator) {
-    println!("The result of Addition is : {}", item.add());
-}*/
-
 pub fn calculate<T: Calculator> (item: T) {
     println!("The result of Addition is : {}", item.add());
 }
+
+/*
+//This is also same but with differenrt syntax
+
+pub fn calculate(item: impl Calculator) {
+    println!("The result of Addition is : {}", item.add());
+}*/
+
 
